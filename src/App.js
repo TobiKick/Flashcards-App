6e5b83f8e444887fa2c53731ff32d5787365dee6
Tobias Kick <kick.tobias@gmx.de>
2020-05-02 01:00:50 +0800

    Added Firebase

--- src/App.js
+++ src/App.js
@@ -3,29 +3,47 @@ import './App.css';
 import Card from './Card/Card';
 import DrawButton from './DrawButton/DrawButton';
 
+import firebase from 'firebase/app';
+import 'firebase/database';
+
+import { DB_CONFIG } from './Config/Firebase/db_config';
+
 class App extends Component {
   constructor(props){
     super(props);
 
+    this.app = firebase.initializeApp(DB_CONFIG)
+    this.database = this.app.database().ref().child('cards')
+    
     // This binding is necessary to make `this` work in the callback
     this.updateCard = this.updateCard.bind(this);
 
     this.state = {
-        cards: [
-            {id: 1, eng: "English", han: "Hanzi", pin: "Pinyin"},
-            {id: 2, eng: "English_2", han: "Hanzi_2", pin: "Pinyin_2"},
-        ],
+        cards: [],
         currentCard: {}
     }
   }
 
   componentWillMount(){
+    // Connect to Firebase + get the current cards from there
     const currentCards = this.state.cards;
 
-    this.setState({
+    this.database.on('child_added', snap => {
+      
+      currentCards.push({
+        id: snap.key,
+        eng: snap.val().eng,
+        han: snap.val().han,
+        pin: snap.val().pin,
+      })
+
+      this.setState({
         cards: currentCards,
         currentCard: this.getRandomCard(currentCards)
+      })
     })
+
+    
   }
 
   getRandomCard(currentCards){
